5e5eeb247584ce293bedbeab1a21a945fc7748b6
iks1 <rajinertia9239111@gmail.com>
2023-06-27 01:02:03 +0530

    fixed header

--- src/components/Headercomponents/SearchBar.jsx
+++ src/components/Headercomponents/SearchBar.jsx
@@ -5,7 +5,7 @@ const SearchBar = () => {
     return (
         <div className='bg-[#F5F5F5] flex flex-row items-center w-auto h-[5vh] min-[320px]:p-3 lg:p-0'>
             <img className=' w-[2.5vh]' src={search} alt='search logo' />
-            <input className='focus:outline-none bg-transparent text-[#787486] text-[14px] m-3' placeholder='Search for anything' type='text'/>
+            <input className='focus:outline-none overflow-hidden bg-transparent text-[#787486] text-[14px] m-3' placeholder='Search for anything fiasdhfasnfioafniasdmnfashfkjasnfjkahsfkjnasdnfjasnfakjsnfkasndfn' type='text'/>
         </div>
     )
 }
